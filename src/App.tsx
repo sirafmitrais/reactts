e6f8d50eaac37a3b2bb2574ac93d12a74569c2ca
muhammad faris <rizkymuhammad.farisprakoso@mitrais.com>
2021-07-10 15:08:34 +0700

    feat: add state initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,67 @@
 import React from 'react';
+import { useCallback, useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { isTemplateExpression } from 'typescript';
+
+const Heading = ({title}:{title: string},{name}:{name: string}) => (
+  <h2>
+    {title}
+  </h2>
+)
+
+const Box:React.FunctionComponent = ({ children }) => (
+  <div
+    style={{
+      padding: '1rem',
+      fontWeight: "bold",
+    }}
+  >
+    {children}
+  </div>
+);
+
+const List: React.FunctionComponent<{
+  items: string[];
+  onClick?: (item: string) => void;
+}> = ({ items }) => (
+  <ul>
+    {items.map((item, index) => (
+      <li key={index}>{item}</li>
+    ))}
+  </ul>
+)
+
+type peopleData = {
+  "name":string,
+  "age":number,
+  "job":string
+}
+
+interface peoplePayload {
+  peopleList : peopleData[];
+}
+
 
 function App() {
+  const onListClick = useCallback((item: string) => {
+    alert(item)
+  }, []);
+
+
+
+  const [people, setPeople] = useState<peoplePayload | null>(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Heading title="Baracode"></Heading>
+      <Box>
+        Hello there
+      </Box>
+      <List items={["one","two","three"]} onClick={onListClick}/>
+      <Box>
+
+      </Box>
     </div>
   );
 }
